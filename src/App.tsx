d4698d729d3ebcc35eeeed7240a3010d7ebabef8
Dmitrii Goroshuk <goroshuk@yandex.ru>
2024-07-13 12:50:27 +0300

    asd

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,30 @@
-import {useState} from "react";
-import {Link, Routes, Route} from "react-router-dom";
-import {About} from "./pages/About";
-import {Welcome} from "./pages/Welcome";
+import { useState } from "react";
+import { Link, Routes, Route } from "react-router-dom";
+import { About } from "./pages/About";
+import { Welcome } from "./pages/Welcome";
 
 export const App = () => {
-    const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-    const increment = () => setCount(prev => prev + 1);
-    const decrement = () => setCount(prev => prev - 1);
+  const increment = () => setCount((prev) => prev + 1);
+  const decrement = () => setCount((prev) => prev - 1);
 
-    return (
-        <div>
-            <h1>value = {count}</h1>
-            <button onClick={increment}>increment</button>
-            <button onClick={decrement}>decrement</button>
+  return (
+    <div>
+      <h1>value = {count * 5}</h1>
+      <h1>HA!</h1>
+      <button onClick={increment}>increment</button>
+      <button onClick={decrement}>decrement</button>
 
-            <div style={{display: 'flex', gap: 15}}>
-                <Link to={'about'}>about</Link>
-                <Link to={'welcome'}>welcome</Link>
-            </div>
+      <div style={{ display: "flex", gap: 15 }}>
+        <Link to={"about"}>about</Link>
+        <Link to={"welcome"}>welcome</Link>
+      </div>
 
-            <Routes>
-                <Route path="/about" element={<About />} />
-                <Route path="/welcome" element={<Welcome />} />
-            </Routes>
-        </div>
-    );
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/welcome" element={<Welcome />} />
+      </Routes>
+    </div>
+  );
 };
-
